9aaf8c64d58acfdb7e7fac639a987a6d1d7d4474
Alexander <myemail@m.ru>
2024-04-18 17:09:40 +0400

    get_top_100 endpoint was made with tests

--- tests/repos/routers_test.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from sqlalchemy.ext.asyncio import AsyncSession
-
-
-async def test_get_top_100(repos_url: str, async_client: AsyncSession, some_repo_added: None):
-	
-	result = await async_client.get(url=repos_url + 'top100')
-	print(result.json())

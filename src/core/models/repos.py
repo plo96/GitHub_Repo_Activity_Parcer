a428118ab135b7d1871060aaf0a857070075bee1
plo96 <plo96@mail.ru>
2024-04-25 07:52:36 +0300

    small bugs fixed

--- src/core/models/repos.py
+++ src/core/models/repos.py
@@ -9,7 +9,7 @@ from .base import Base
 from .get_default import get_str_uuid
 
 DEFAULT_SORT_PARAM = "stars"				# Параметр по умолчанию для сортировки репозиториев при чтении из БД
-LIMIT_TOP_REPOS_LIST = 100					# Число репозиториев в топе (БД)
+LIMIT_TOP_REPOS_LIST = 5					# Число репозиториев в топе (БД)
 
 
 class Repo(Base):
